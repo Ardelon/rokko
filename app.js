0141226ed0f474591d1c2b50f6dc136fe2ed4c19
ardelon <akeskinw@gmail.com>
2022-08-23 20:50:17 +0300

    -

--- app.js
+++ app.js
@@ -2,13 +2,8 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const app = express();
-const server = http.createServer(app);
 require('dotenv').config()
-app.use(express.static('dist'));
-
-app.get('', (req, res) => {
-	res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.use(express.static('/root/rokko/dist')); // prod
 
 app.get('/rss', (req, res) => {
   res.contentType('application/xml');
@@ -20,8 +15,7 @@ app.get('/test-episode', (req, res) => {
   res.sendFile(path.join(__dirname , 'public/rokko-test.mpeg'));
 	
 });
-server.listen(process.env.PORT || 5000, () => console.log(`Server running on port 5000`));
-// const server = app.listen(process.env.PORT || 5000, () => {
-// 	var port = server.address().port;
-// 	console.log('Listening...' + port);
-// });
+const server = app.listen(process.env.PORT || 3310, () => {
+  var port = server.address().port;
+  console.log('Listening...' + port);
+});
